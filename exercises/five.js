9651be8feb11b7fc1a7a5389c799e0c6230fc911
22miuel <22miguel.gallardo22@gmail.com>
2024-05-31 16:27:54 -0400

    End this first exercises to oddin'

---
+++ exercises/five.js
@@ -0,0 +1,7 @@
+const MAX = 57
+
+let actual = MAX - 13
+
+let porcentaje = actual / MAX
+
+console.log(porcentaje)
---
+++ exercises/four.js
@@ -0,0 +1,9 @@
+let a = 10;
+
+console.log(a)
+
+9 * a
+
+let b = 7 * a;
+
+console.log(b)
